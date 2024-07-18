4cd4717d9f760a9a8f967d28bc356ab04d7095f6
MinhNhat214 <nhattran200411@gmail.com>
2024-07-18 16:10:56 -0700

    navigation update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -14,9 +14,9 @@
                 class="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">
                 Đăng ký
             </a>
-            <a href="#"
+            {{-- <a href="#"
                 class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Get
-                started</a>
+                started</a> --}}
             <button data-collapse-toggle="mobile-menu-2" type="button"
                 class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                 aria-controls="mobile-menu-2" aria-expanded="false">
